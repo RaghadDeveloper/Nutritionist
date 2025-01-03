547aec718aa6aff99e3b00a2715de675293cd3bb
leenfedah <alifedah106@gmail.com>
2025-01-03 15:21:35 +0300

    task title

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './Testimonials.css'
-
+import TestimCard from '../TestimCard/TestimCard'
+import { TestimData } from '../Data/TestimData'
+import img5 from './../../assets/images/home/Indicators.png'
+import img6 from './../../assets/images/home/left arrow.png'
+import img7 from './../../assets/images/home/right arrow.png'
 export default function Testimonials() {
   return (
-    <div>Testimonials</div>
+    <>
+    <div className=' LF-padding d-flex jutify-content-between'>
+   {TestimData.map((e,index) => {
+    return(
+      < TestimCard key={index} title={e.title} img={e.img} desc={e.desc} imge={e.imge}/>
+    )
+
+   })}</div>
+   <div className='LF-shape '>
+   <img src={img6 }/>
+   <img src={img5}  />
+   <img src={img7}/>
+  </div>
+  </>
+
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Testimonials from '../../Components/Testimonials/Testimonials'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+   <Testimonials/>
+    </>
   )
 }
