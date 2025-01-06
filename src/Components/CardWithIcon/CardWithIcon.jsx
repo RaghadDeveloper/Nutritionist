63a767ce405e18457e06e13d4318e45989266fec
hanin222 <haninomar@gmail.com>
2025-01-06 21:05:48 +0300

    font size updated

--- src/Components/CardWithIcon/CardWithIcon.jsx
+++ src/Components/CardWithIcon/CardWithIcon.jsx
@@ -6,8 +6,11 @@ const CardWithIcon = ({ image, title, text }) => {
   return (
     <div className="card">
       <div className="image-container">
-        <img src={image}  />
-        <h3>{title}</h3>
+      <img src={image}  />
+       <div className="cont">
+       
+       <h3>{title}</h3>
+       </div>
       </div>
       <div className="card-content">
         <p>{text}</p>
