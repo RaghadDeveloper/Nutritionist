6ddd787481c1634fb1f25b64827e096c92d32f20
wafaa-aa <fyfy3thman@gmail.com>
2025-01-11 21:16:40 +0200

    Last-Update-EndConting

--- src/Components/EndContent/EndContent.jsx
+++ src/Components/EndContent/EndContent.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './EndContent.css'
-import AbstractDesign2 from "./../../assets/images/footer/Abstract Design3.png"
 import AbstractDesign from "./../../assets/images/footer/Abstract Design 2.png"
+import AbstractDesign2 from "./../../assets/images/footer/Abstract Design 3.png"
+import AbstractDesign3 from "./../../assets/images/footer/Abstract Design 4.png"
+import AbstractDesign4 from "./../../assets/images/footer/Abstract Design 5.png"
+
+
 
 export default function EndContent({ text, desqription }) {
   return (
@@ -11,6 +15,8 @@ export default function EndContent({ text, desqription }) {
         <img src={AbstractDesign} className='WO-AbstractDesign1' />
 
         <img src={AbstractDesign2} className='WO-AbstractDesign2' />
+        <img src={AbstractDesign3} className='WO-AbstractDesign3' />
+        <img src={AbstractDesign4} className='WO-AbstractDesign4' />
         <div className='WO-EndContainer'>
           <div className='WO-TextContainer1'>
             <h2>{text}</h2>
