1e0970255f26cbd3e33b451ddcafa39da297ec15
NaghamGawad6767 <naghamgawad27@gmail.com>
2025-01-14 19:21:11 +0300

    therd commit

---
+++ src/Components/Data/faqData.jsx
@@ -0,0 +1,33 @@
+export const title = 'FAQ'
+export const text = 'We address commonly asked questions and provide comprehensive answers to help you navigate your way through your nutrition and wellness journey.'
+
+
+const faqData = [
+    {
+      question: "What is personalized nutrition coaching?",
+      answer: "Personalized nutrition coaching involves creating a customized nutrition plan based on your unique goals, preferences, and dietary requirements. Our team of experts will work closely with you to develop a plan that fits your lifestyle and helps you achieve optimal health and wellness.",
+    },
+    {
+      question: "How can nutrition coaching help me lose weight?",
+      answer: "Nutrition coaching helps you adopt healthier eating habits and create a balanced diet tailored to your weight loss goals.",
+    },
+    {
+      question: "Can I still enjoy my favorite foods while on a nutrition plan?",
+      answer: "Yes, moderation is key, and a good nutrition plan can accommodate your favorite foods while maintaining overall balance.",
+    },
+    {
+      question: "How often will I have contact with my nutritionist?",
+      answer: "Contact frequency varies but can be customized based on your needs and preferences.",
+    },
+    {
+      question: "How long will it take to see results?",
+      answer: "Results depend on your goals and adherence to the nutrition plan. Typically, noticeable results appear in a few weeks.",
+    },
+    {
+      question: "Are the meal plans and recipes provided suitable for vegetarians or vegans?",
+      answer: "Yes, meal plans can be customized to suit vegetarian and vegan preferences.",
+    },
+  ];
+  
+  export default faqData;
+  
--- src/Components/FAQ/FAQ.jsx
+++ src/Components/FAQ/FAQ.jsx
@@ -1,28 +1,23 @@
 import React from 'react'
 import './FAQ.css'
-import img from './../../assets/images/pricing/Logo.png'
+import SectionHeader from '../SectionHeader/SectionHeader'
+import { title, text } from "./../Data/faqData.jsx"
+import FaqCard from '../FaqCard/FaqCard.jsx'
+import FaqQuestion from '../FaqQuestion/FaqQuestion/FaqQuestion.jsx'
+
 export default function FAQ() {
   return (
-    <div className='NG-FAQ'>
-      <div className='NG-first'>
-        <h3>FAQ</h3>
-        <p>We address commonly asked questions and provide comprehensive answers to help you navigate your way through your nutrition and wellness journey.</p>
-      </div>
-      <div className='NG-first1'>
-        <div className='NG-left1'>
-         
+    <section className='main-container '>
+      <SectionHeader title={title} text={text} />
+      <div className="NG-faq-container" >
+        <div className="NG-faq-questions">
+          <FaqQuestion />
         </div>
-        <div className='NG-right1'>
-          <img src={img} alt="logo" />
-          <div className='NG-text'>
-            <h5>Ask your question</h5>
-            <p>Feel Free to Ask questions on anytime</p>
-          </div>
-          <div className='NG-BTN'>
-            <button>Ask A Question</button>
-          </div>
-        </div>
+        <FaqCard />
       </div>
-    </div>
+    </section>
   )
 }
+
+
+
