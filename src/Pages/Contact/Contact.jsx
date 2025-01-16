49d2eb41d7b870b96f4b02643a1e1a9fde775bb8
NaghamGawad6767 <naghamgawad27@gmail.com>
2025-01-16 18:05:20 +0300

    project first

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Contact.css'
+import Hero from '../../Components/Hero/Hero/Hero'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <Hero title={"Contact Us"} description={"We value your feedback, questions, and concerns at Nutritionist. Our dedicated team is here to assist you and provide the support you need on your nutritional journey. Please don't hesitate to reach out to us using any of the following contact methods"} />
+    </>
   )
 }
--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Pricing.css'
 import FAQ from '../../Components/FAQ/FAQ'
+import Hero from '../../Components/Hero/Hero/Hero'
 
 export default function Pricing() {
   return (
-    <FAQ />
+    <>
+      <Hero title={"Our Pricing"} description={"At Nutritionist, we offer flexible pricing options to accommodate your unique requirements and budget. Our goal is to provide you with exceptional personalized nutrition coaching that is accessible and tailored to your needs. Choose from our three plans below and take the first step towards a healthier lifestyle"}/>
+      <FAQ />
+    </>
+
   )
 }
