6d0062d7550cedac27b4f3f05295f3e3347d3d44
wafaa-aa <fyfy3thman@gmail.com>
2025-01-18 17:00:57 +0200

    Animation-for-4Section

--- src/Components/EndContent/EndContent.jsx
+++ src/Components/EndContent/EndContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './EndContent.css'
 import AbstractDesign from "./../../assets/images/footer/Abstract Design 2.png"
 import AbstractDesign2 from "./../../assets/images/footer/Abstract Design 3.png"
@@ -6,12 +6,26 @@ import AbstractDesign3 from "./../../assets/images/footer/Abstract Design 4.png"
 import AbstractDesign4 from "./../../assets/images/footer/Abstract Design 5.png"
 
 
-
 export default function EndContent({ text, desqription }) {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.5 });
+
+    document.querySelectorAll('.fade-slide-up').forEach(element => {
+      observer.observe(element);
+    });
+  }, []);
+
   return (
     <div className=' main-container'>
 
-      <div className='EndContent'>
+      <div className='EndContent fade-slide-up'>
         <img src={AbstractDesign} className='WO-AbstractDesign1' />
 
         <img src={AbstractDesign2} className='WO-AbstractDesign2' />
