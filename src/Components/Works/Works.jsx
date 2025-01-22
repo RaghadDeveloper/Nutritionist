3f22f863ebd3fb1aec192f9159072f016a181914
hanin222 <haninomar@gmail.com>
2025-01-22 15:32:23 +0300

    work sec v3

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -1,118 +1,17 @@
 import React from "react";
 import "./Works.css";
 import CardWithIcon from "../CardWithIcon/CardWithIcon";
-
-import Icon1 from "../../assets/images/process/Icon 1.png";
-import Icon2 from "../../assets/images/process/Icon 2.png";
-import Icon3 from "../../assets/images/process/Icon 3.png";
-import Icon4 from "../../assets/images/process/Icon 4.png";
-import Icon5 from "../../assets/images/process/Icon 5.png";
-import Icon6 from "../../assets/images/process/Icon 6.png";
-import Icon7 from "../../assets/images/process/Icon 7.png";
-import Icon8 from "../../assets/images/process/Icon 8.png";
-import Icon9 from "../../assets/images/process/Icon 9.png";
-import Icon10 from "../../assets/images/process/Icon 10.png";
-
-import num1 from "../../assets/images/process/Number 1.png";
-import num2 from "../../assets/images/process/Number 2.png";
-import num3 from "../../assets/images/process/Number 3.png";
-import num4 from "../../assets/images/process/Number 4.png";
-import num5 from "../../assets/images/process/Number 5.png";
-import num6 from "../../assets/images/process/Number 6.png";
-import num7 from "../../assets/images/process/Number 7.png";
-import num8 from "../../assets/images/process/Number 8.png";
-import num9 from "../../assets/images/process/Number 9.png";
-import num10 from "../../assets/images/process/Number 10.png";
+import SectionHeader from "../SectionHeader/SectionHeader";
+import { workdata, title, text } from "../Data/workdata"; 
 
 export default function Works() {
-  const cardsData = [
-    {
-      image: Icon1,
-      title: "Initial Consultation",
-      text: "We start by scheduling an initial consultation, either in person or through a convenient online meeting. During this session, we will discuss your health history, lifestyle, goals, and any specific dietary requirements.",
-      showNumber: true,
-      numberImage: num1,
-    },
-    {
-      image: Icon2,
-      title: "Assessing Your Needs",
-      text: "Our team of experienced nutritionists and dietitians will conduct a thorough assessment of your nutritional needs and create a personalized plan tailored to your unique requirements.",
-      showNumber: true,
-      numberImage: num2,
-    },
-    {
-      image: Icon3,
-      title: "Personalized Nutrition Plan",
-      text: "Based on the information gathered, we will develop a personalized nutrition plan that takes into account your dietary preferences, lifestyle, and health goals. This plan will provide you with clear guidelines on what to eat, portion sizes, and meal timing.",
-      showNumber: true,
-      numberImage: num3,
-    },
-    {
-      image: Icon4,
-      title: "Meal Planning and Recipes",
-      text: "To make your journey easier, we will provide you with a variety of delicious and nutritious meal options, along with recipes that align with your personalized nutrition plan.",
-      showNumber: true,
-      numberImage: num4,
-    },
-    {
-      image: Icon5,
-      title: "Ongoing Support",
-      text: "Throughout your journey, our team of nutrition experts will be there to provide ongoing support, answer your questions, and offer guidance. We believe in the power of continuous encouragement to help you stay motivated and achieve long-term success.",
-      showNumber: true,
-      numberImage: num5,
-    },
-    {
-      image: Icon6,
-      title: "Progress Tracking",
-      text: "We encourage you to track your progress using our user-friendly mobile app or website tools. You can log your meals, monitor your weight, and track your physical activity, allowing you to have a clear picture of your progress and make any necessary adjustments.",
-      showNumber: true,
-      numberImage: num6,
-    },
-    {
-      image: Icon7,
-      title: "Regular Check-ins",
-      text: "We will schedule regular check-ins to review your progress, address any concerns, and make any necessary adjustments to your nutrition plan. Our goal is to ensure that you are consistently moving towards your desired outcomes.",
-      showNumber: true,
-      numberImage: num7,
-    },
-    {
-      image: Icon8,
-      title: "Education and Resources",
-      text: "Along the way, we will provide you with educational resources, such as articles, guides, and videos, to enhance your understanding of nutrition and empower you to make informed choices for a healthier lifestyle.",
-      showNumber: true,
-      numberImage: num8,
-    },
-    {
-      image: Icon9,
-      title: "Fine-Tuning and Adjustments",
-      text: "As your journey progresses and your goals evolve, we will fine-tune and make adjustments to your nutrition plan to ensure that it continues to align with your changing needs.",
-      showNumber: true,
-      numberImage: num9,
-    },
-    {
-      image: Icon10,
-      title: "Sustainable Lifestyle Habits",
-      text: "Our ultimate goal is to help you develop sustainable lifestyle habits that will support your long-term health and wellness. We will equip you with the knowledge and skills to maintain your progress even after you've completed the program.",
-      showNumber: true,
-      numberImage: num10,
-    },
-  ];
   return (
     <div>
-      <div className="works-section container">
-        <div className="hed">
-          <h2>How It Works</h2>
-          <p>
-            We provide a step-by-step guide on how to get started on your
-            journey towards better health and nutrition. We are here to simplify
-            the process and make it easy for you to navigate our platform and
-            access the resources you need to achieve your goals. Here's how it
-            works
-          </p>
-        </div>
-        <div className="work-wrapper ">
+      <div className="works-section main-container">
+        <SectionHeader title={title} text={text} />
+        <div className="work-wrapper">
           <div className="cards-container">
-            {cardsData.map((card, index) => (
+            {workdata.map((card, index) => (
               <CardWithIcon
                 key={index}
                 image={card.image}
