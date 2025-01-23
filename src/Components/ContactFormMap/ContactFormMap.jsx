6f8df65b2d031680c2bd650bec78c377d14a0954
rahaf <rahafesmael13.7@gmail.com>
2025-01-23 06:42:47 +0300

    FormAndMap

--- src/Components/ContactFormMap/ContactFormMap.jsx
+++ src/Components/ContactFormMap/ContactFormMap.jsx
@@ -1,35 +1,54 @@
-import React from 'react'
-import './ContactFormMap.css'
+import './Contact.css'
 
-export const ContactFormMap = () => {
-  return (
-    <section>
-           
-    <div className='thirdPart'>
-        <form className='formRS'>
-            <div className='flexForm'>
-                <span >
-                    <label>Full Name</label>
-                    <input type='text' placeholder='Enter Your Name'/>
-                </span>
-                <span>
-                    <label>Email</label>
-                    <input type='email' placeholder='Enter your Email'/>
-                </span>    
-            </div>
-            <label>Phone Number</label>
-            <input type='number' placeholder='Enter your Number'/>
-            <label>Message</label>
-            <textarea type='text' placeholder='Enter your Message'/>
-            <p>Max 250 Chars</p>
-            <button>Send Message</button>
-        </form>
-        <div className='mapRS'>
-            <img src='/img/Map.jpg' alt='...'/>
-            <button><img src='/img/Buttons Container.svg' alt='...' /></button>
-        </div>
-    </div>
+function Contact (){
+    const handleFacebookClick =() =>{
+         window.open('https://www.facebook.com/' , '_blank');
+    };
+    const handleTwitterClick = () =>{
+        window.open('https://www.twitter.com/' , '_blank')
+    } 
+    const handleLinkedInClick = () =>{
+        window.open('https://www.linkedin.com/' , '_blank')
+    } 
+    //const[inputValue,setInputValue]=useState('');
+    //const handleSubmite = (event)=>{event.preventDefaulte();
+    //console.log('inputValue')}
+    return (
+        <>
+            <section className='thirdPart'>
+                <form className='formRS'>
+                    <div className='flexForm'>
+                        <span >
+                            <label>Full Name</label>
+                            <input type='text' placeholder='Enter Your Name' />
+                        </span>
+                        <span>
+                            <label>Email</label>
+                            <input type='email' placeholder='Enter your Email'/>
+                        </span>    
+                    </div>
+                    <label>Phone Number</label>
+                    <input type='number' placeholder='Enter your Number'/>
+                    <label>Message</label>
+                    <textarea type='text' placeholder='Enter your Message'/>
+                    <p>Max 250 Chars</p>
+                    <button>Send Message</button>
+                </form>
+                <div className='mapRS'>
+                    <img src='./public/img/Map.jpg' alt='...' className='map'/>
+                  <div className='btnRS'>  
+                    <button onClick={handleFacebookClick}><img src='./public/img/Button.png' alt='...' /></button>
+                    <button onClick={handleTwitterClick}><img src='./public/img/Button(1).png' alt='...' /></button>
+                    <button onClick={handleLinkedInClick}><img src='./public/img/Button(2).png' alt='...' /></button>
+                  </div>
+                </div>
+            
+            
+        </section>
+        
+        </>
+        )
+
     
-</section>
-  )
 }
+export default Contact
