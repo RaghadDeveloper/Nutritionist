719879d7aeb5483ac957ceadb90c078716b43c95
wafaa-aa <fyfy3thman@gmail.com>
2025-01-23 12:14:05 +0200

    Update NavBar-Footer

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,75 +1,108 @@
 import "./NavBar.css";
-// import { Link } from 'react-router-dom';
+import React, { useState } from "react";
 import Abstract from "../../assets/images/nav/Abstract Design 1.png";
 import Abstract2 from "../../assets/images/nav/Abstract Design 2.png";
+import Abstract3 from "../../assets/images/nav/Abstract Design 3.png";
+import Abstract4 from "../../assets/images/nav/Abstract Design 4.png";
+import Abstract5 from "../../assets/images/nav/Abstract Design 5.png";
+import Abstract6 from "../../assets/images/nav/Abstract Design 6.png";
 import cricketball from "../../assets/images/nav/cricket ball.png";
-import logo from "../../assets/images/nav/Logo.png";
-import icone from "../../assets/images/nav/Icon.png";
-
 import rightarrow from "../../assets/images/nav/right arrow.png";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../../assets/images/nav/Logo.png"
 import { Link } from "react-router-dom";
+import BoxIcon from "../../assets/images/nav/Icon.png"
+import { links } from "../Data/LinksData";
+
+
+
 export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("/");
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleLinkClick = (path) => {
+    setActiveLink(path);
+    setMenuOpen(false);
+  };
+
   return (
     <>
       <nav>
-        <div className="logo">
+        <img className=" SH-Abstract-left3" src={Abstract5} alt="" />
+        <div className="SH-logo">
           <img className=" SH-Abstract-left" src={Abstract} alt="" />
+          <img className=" SH-Abstract-left2" src={Abstract3} alt="" />
           <div className="navInfo">
             <img className="SH-cricketball" src={cricketball} alt="" />
             <p>Join Our Personalized Nutrition Demo For Free</p>
-            <img className="SH-rightarrow" src={rightarrow} alt="" />
           </div>
-
+          <img className="SH-rightarrow" src={rightarrow} alt="" />
           <img className=" SH-Abstract-right" src={Abstract2} alt="" />
+          <img className=" SH-Abstract-right2" src={Abstract4} alt="" />
         </div>
+        <img className=" SH-Abstract-right3" src={Abstract6} alt="" />
       </nav>
-      <Navbar expand="lg" className="bg-body-tertiary sh-Navbar">
-        <Container fluid>
-          <img className=" sh-img-logo" src={logo} alt="" />
-          {/* <img src={icone} alt="" /> */}
-          <Navbar.Toggle aria-controls="navbarScroll" style={{background:"#cbea7b"}}/>
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="ms-auto my-2 my-lg-0 -primary-text  SH-navLINKS"
-              navbarScroll
+      <div className="NavBar">
+        <img className="WO-Logo" src={logo} />
+        <div className="Wo-RightSide" style={{ display: menuOpen ? "flex" : "none" }}>
+          <ul className="WO-Menu mb-0">
+            {links.slice(0, 6).map((link) => (
+              <li key={link.path}>
+                <Link
+                  to={link.path}
+                  className={activeLink === link.path ? "WO-Active1" : ""}
+                  onClick={() => handleLinkClick(link.path)}
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <button className="WO-Button">
+
+            <Link
+              to="/contact"
+              className={activeLink === "/contact" ? "WO-Active1" : ""}
+              onClick={() => handleLinkClick("/contact")}
             >
-              <Link className="SH-LINK-LIGHT" to="/">
-                Home
-              </Link>
-              <Link className="SH-LINK" to="/about">
-                About
-              </Link>
-              <Link className="SH-LINK" to="/team">
-                Team
-              </Link>
-              <Link className="SH-LINK" to="/process">
-                Process
-              </Link>
-              <Link className="SH-LINK" to="/pricing">
-                Pricing
-              </Link>
-              <Link className="SH-LINK" to="/blog">
-                Blog
-              </Link>
-            </Nav>
-            <Form action="">
-              <Button className="sh-nav-btn" variant="outline-success">
-                Contact Us
-              </Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+              Contact Us
+            </Link>
+
+          </button>
+
+        </div>
+        <img className="BoxIcon" src={BoxIcon} alt="Menu Icon" onClick={toggleMenu} />
+      </div>
+      {menuOpen && (
+        <div className="mobile-menu">
+          <ul className="WO-Menu mb-0">
+            {links.slice(0, 6).map((link) => (
+              <li key={link.path}>
+                <Link
+                  to={link.path}
+                  className={activeLink === link.path ? "WO-Active1" : ""}
+                  onClick={() => handleLinkClick(link.path)}
+                >
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <button className="WO-Button">
+            <Link
+              to="/contact"
+              className={activeLink === "/contact" ? "WO-Active1" : ""}
+              onClick={() => handleLinkClick("/contact")}
+            >
+              Contact Us
+            </Link>
+          </button>
+        </div>
+      )}
+
 
     </>
   );
