2ab4609f1e3d47e26e9cce62bc62a29c0d03d493
soos545 <sanmubayed@gmail.com>
2025-01-26 01:02:54 +0300

    update on pricingFeatures

--- src/Components/PriceTableRow/PriceTableRow.jsx
+++ src/Components/PriceTableRow/PriceTableRow.jsx
@@ -1,141 +1,36 @@
-// import { Col, Row } from 'react-bootstrap';
 import imgTrue from "../../assets/images/pricing/true.png";
 import imgFalse from "../../assets/images/pricing/false.png";
 import "./PriceTableRow.css";
-export default function PriceTableRow({
-  titlePircing1,
-  titlePircing2,
-  titlePircing3,
-  titlePircing4,
-  infoPircing,
-  infoPircing1,
-  infoPircing2,
-  infoPircing3,
-  infoPircing4,
-  infoPircing5,
-  infoPircing6,
-  infoPircing7,
-  infoPircing8,
-  infoPircing9,
-  // img1,
-}) {
+export default function PriceTableRow({ tableData }) {
   return (
-    <section className="sh-cards  d-flex">
-      <div className="sh-card-feature d-flex flex-direction-column">
-        <h5 className="sh-card-title ">{titlePircing1}</h5>
-
-        <p>{infoPircing}</p>
-        <p>{infoPircing1}</p>
-        <p>{infoPircing2}</p>
-        <p>{infoPircing3}</p>
-        <p>{infoPircing4}</p>
-        <p>{infoPircing5}</p>
-        <p>{infoPircing6}</p>
-        <p>{infoPircing7}</p>
-        <p>{infoPircing8}</p>
-        <p>{infoPircing9}</p>
-      </div>
-      <div className="sh-card-plane d-flex flex-direction-column">
-        <h5 className="sh-card-title ">{titlePircing2}</h5>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-                <div className="sh-tf-img">
-
-          <img src={imgFalse} alt="" />
-          </div>
-      </div>
-      <div className="sh-card-plane d-flex flex-direction-column">
-        <h5 className="sh-card-title ">{titlePircing3}</h5>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgFalse} alt="" />
-        </div>
-                <div className="sh-tf-img">
-
-          <img src={imgFalse} alt="" />
-          </div>
-      </div>
-      <div className="sh-card-plane-last d-flex flex-direction-column">
-        <h5 className="sh-card-title ">{titlePircing4}</h5>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-        <div className="sh-tf-img">
-          <img src={imgTrue} alt="" />
-        </div>
-                <div className="sh-tf-img">
-
-          <img src={imgTrue} alt="" />
-          </div>
+    <section className="sh-sectiontabel">
+      <div className="sh-innertable">
+        <table className="w-100 sh-table">
+          <thead>
+            <tr>
+              <th>
+                <div className="sh-cardheader text-start ms-0">Features</div>
+              </th>
+              {tableData.plans.map((plan, index) => (
+                <th key={index} className="sh-left text-center">
+                  <div className="sh-cardheader">{plan.name}</div>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="sh-tbody">
+            {tableData.features.map((feature, index) => (
+              <tr className="sh-tr" key={index}>
+                <td className="sh-feature sh-bottom">{feature}</td>
+                {tableData.plans.map((plan, planIndex) => (
+                  <td className="sh-check sh-bottom sh-left" key={planIndex}>
+                    < img className="img-status" src={plan.features[index] ? imgTrue : imgFalse} alt="" />
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </section>
   );
