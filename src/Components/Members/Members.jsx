3add3693f9a8bbf048808afcd57a80b51e40bf5c
leenfedah <alifedah106@gmail.com>
2025-01-25 12:43:38 +0300

    member

--- src/Components/Members/Members.jsx
+++ src/Components/Members/Members.jsx
@@ -11,42 +11,6 @@ export default function Members() {
  
   return (
     <>
-   
-   <Tab.Container className="left-tabs-example" defaultActiveKey="first">
-   <Row >
-       
-          <Nav variant="pills" className="flex-Row LF-color">
-           <Col sm={1}></Col>
-          <Col sm={2} >
-            <Nav.Item  className='LF-item1'>
-              <Nav.Link eventKey="first"className='LF-line1'  >Management Team</Nav.Link>
-            </Nav.Item>
-            </Col>
-            <Col sm={2}>            
-            <Nav.Item className='LF-item2'>
-              <Nav.Link eventKey="second" className='LF-line2 '>Nutritionists and Dietitians </Nav.Link>
-            </Nav.Item>
-          </Col>
-          <Col sm={2}>
-            <Nav.Item className='LF-item3'>
-              <Nav.Link eventKey="third"className='LF-line3 '>Customer Support</Nav.Link>
-            </Nav.Item>
-          </Col>
-          <Col sm={2}>
-            <Nav.Item className='LF-item4'>
-              <Nav.Link eventKey="fourth"className='LF-line4 '>Marketing and Communications</Nav.Link>
-            </Nav.Item>
-          </Col>
-          <Col sm={2}>
-            <Nav.Item className='LF-item5'>
-              <Nav.Link eventKey="fivth"className='LF-line5 '>Technology and Development</Nav.Link>
-            </Nav.Item>
-          </Col>
-          <Col sm={1}></Col>
-          
-          </Nav>
-      </Row>
-      </Tab.Container>
      
       <div className='LF-cardMEM main-container '>
       {MemberData.map((e,index) => {
